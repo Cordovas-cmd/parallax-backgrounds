45fb9e487f12f55f32e8e20a1f155573a6d835a4
Samuel Cordova <cordovas1337@gmail.com>
2024-07-21 19:17:47 -0500

    added a cube and tested movement along the x axis

--- script.js
+++ script.js
@@ -10,3 +10,29 @@ const CANVAS_WIDTH = canvas.width = 600;
 const CANVAS_HEIGHT = canvas.height = 600;
 
 // bring image in to JS
+const playerImage = new Image();
+playerImage.src = './images/shadow_dog.png';
+let x = 0;
+
+// function to start animation loop
+function animate() {
+    // clear whole canvas
+    ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+
+    ctx.fillRect(x,50,100,100);
+    x++
+    
+
+    // run a function we pass to it, since I'm calling the animate function it will run it over and over. 
+    requestAnimationFrame(animate);
+
+
+    // if statement to reset and reloop x
+    // if(x > 600){
+    //     // console.log("yes")
+    //     x=0
+    // }
+    
+};
+
+animate();
